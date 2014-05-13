0f85acb6d81ff3cc0e426a6653a189f95cc03edc
Timo <timo_p@ut.ee>
2014-05-13 15:55:45 +0300

    update to etsa importer script

--- hsm/scripts/import_etsa.py
+++ hsm/scripts/import_etsa.py
@@ -7,18 +7,20 @@ from hsm.data.mongosegmentstorage import MongoSegmentStorage
 import sys
 
 def usage():
-    print 'python import_etsa.py [passwd]'
+    print 'python import_etsa.py [port] [passwd]'
     sys.exit()
 
 if __name__ == '__main__':
     args = sys.argv[1:]
-    if len(args) != 1:
+    if len(args) != 2:
         usage()
+    port, passwd = args
+    port = int(port)
     docstorage = MongoDocumentStorage()
     segstorage = MongoSegmentStorage()
     importer = EtsaVisitImporter(
                     host = u'127.0.0.1',
-                    port = 3310,
+                    port = port,
                     user = u'etsad',
                     passwd = args[0],
                     db = u'work',
